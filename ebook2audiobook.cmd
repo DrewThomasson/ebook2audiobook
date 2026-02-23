34c6cc9a2d793e3f280a775c040ea93ce828a942
unknown <admin@radsl.net>
2026-02-23 12:52:34 -0800

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -246,22 +246,21 @@ if errorlevel 1 (
 exit /b 0
 
 :check_scoop
-where.exe /Q scoop
+where.exe /Q scoop >nul 2>&1
 if errorlevel 1 (
     echo Scoop is not installed.
-	exit /b 1
-) else (
-	if exist "%SAFE_SCRIPT_DIR%\.after-scoop" (
-		call "%PS_EXE%" %PS_ARGS% -Command "scoop install git; scoop bucket add muggle https://github.com/hu3rror/scoop-muggle.git; scoop bucket add extras; scoop bucket add versions" || exit /b 1
-		call git config --global credential.helper
-		echo %ESC%[32m=============== Scoop components OK ===============%ESC%[0m
-		findstr /i /x "scoop" "%INSTALLED_LOG%" >nul 2>&1
-		if errorlevel 1 (
-			echo scoop>>"%INSTALLED_LOG%"
-		)
-		del "%SAFE_SCRIPT_DIR%\.after-scoop" >nul 2>&1
-	)
+    exit /b 1
 )
+if not exist "%SAFE_SCRIPT_DIR%\.after-scoop" (
+    echo %ESC%[32m=============== Scoop components OK ===============%ESC%[0m
+    exit /b 0
+)
+call "%PS_EXE%" %PS_ARGS% -Command "scoop install git; scoop bucket add muggle https://github.com/hu3rror/scoop-muggle.git; scoop bucket add extras; scoop bucket add versions" || exit /b 1
+call git config --global credential.helper
+echo %ESC%[32m=============== Scoop components OK ===============%ESC%[0m
+findstr /i /x "scoop" "%INSTALLED_LOG%" >nul 2>&1
+if errorlevel 1 echo scoop>>"%INSTALLED_LOG%"
+del "%SAFE_SCRIPT_DIR%\.after-scoop" >nul 2>&1
 exit /b 0
 
 :check_required_programs
