1d0b0511e681f6947c52d21a1bfe18f53466189e
unknown <admin@radsl.net>
2026-05-01 15:19:47 -0700

    ...

--- lib/classes/tts_engines/xtts.py
+++ lib/classes/tts_engines/xtts.py
@@ -146,7 +146,7 @@ class XTTSv2(TTSUtils, TTSRegistry, name='xtts'):
                                 if self.speaker in default_engine_settings[TTS_ENGINES['XTTSv2']]['voices'].keys():
                                     self.params['gpt_cond_latent'], self.params['speaker_embedding'] = self.xtts_speakers[default_engine_settings[TTS_ENGINES['XTTSv2']]['voices'][self.speaker]].values()
                                 else:
-                                    self.params['gpt_cond_latent'], self.params['speaker_embedding'] = self.engine.get_conditioning_latents(audio_path=[self.params['current_voice']], librosa_trim_db=30, load_sr=24000, sound_norm_refs=True)
+                                    self.params['gpt_cond_latent'], self.params['speaker_embedding'] = self.engine.get_conditioning_latents(audio_path=[self.params['current_voice']], load_sr=24000, sound_norm_refs=True)
                                 self.params['latent_embedding'][self.params['current_voice']] = self.params['gpt_cond_latent'], self.params['speaker_embedding']
                             #with torch.autocast(device, dtype=self.amp_dtype, enabled=(self.amp_dtype != torch.float32)):
                             result = self.engine.inference(
