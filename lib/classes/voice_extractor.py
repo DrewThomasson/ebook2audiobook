c645bfe636cd402a5a275820dace7000667743ff
unknown <admin@radsl.net>
2026-05-04 18:13:10 -0700

    ...

--- lib/classes/voice_extractor.py
+++ lib/classes/voice_extractor.py
@@ -79,7 +79,7 @@ class VoiceExtractor:
             if self.is_gui_process:
                 self.progress_bar(1, desc=msg)
             detector = BackgroundDetector(wav_file=self.wav_file)
-            status, report = detector.detect(vad_ratio_thresh=0.20)
+            status, report = detector.detect(vad_ratio_thresh=0.27)
             if report:
                 print(report)
                 if status:
