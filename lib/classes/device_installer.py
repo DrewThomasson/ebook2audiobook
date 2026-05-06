4dab0240d3c917404fb71644a92f13d7b82b4d25
unknown <admin@radsl.net>
2026-05-06 08:41:34 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -103,7 +103,7 @@ class DeviceInstaller():
 
     def detect_arch_tag(self)->str:
         m = platform.machine().upper()
-        return arch_map.get(m, 'unknown')
+        return archs.get(m, 'unknown')
 
     def detect_device(self)->str:
 
