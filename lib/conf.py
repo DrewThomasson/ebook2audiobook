f2698790e054869d427b3b433e4241d6ecd94b8b
unknown <admin@radsl.net>
2026-05-06 05:44:51 -0700

    ...

--- lib/conf.py
+++ lib/conf.py
@@ -60,38 +60,39 @@ default_py_major = sys.version_info.major
 default_py_minor = sys.version_info.minor
 default_pytorch_url = 'https://download.pytorch.org/whl'
 default_pytorch_amd_url = 'https://repo.radeon.com/rocm/windows'
+default_torchcodec_arm_url = ''
 default_jetson_url = 'https://www.e-blokos.com/whl/jetson' # TODO: find a permanent website where to upload the jetpack torch
 
 torch_matrix = {
     # CPU
     "cpu":       {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0"},
     # CUDA
-    "cu118":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.7.1"},
-    "cu121":     {"compat": list(systems.values()), "base": "2.5.1", "last": "2.5.1"},
-    "cu124":     {"compat": list(systems.values()), "base": "2.6.0", "last": "2.6.0"},
-    "cu126":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0"},
-    "cu128":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0"},
-    "cu129":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0"},
-    "cu130":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0"},
+    "cu118":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.7.1", "codec": ""},
+    "cu121":     {"compat": list(systems.values()), "base": "2.5.1", "last": "2.5.1", "codec": ""),
+    "cu124":     {"compat": list(systems.values()), "base": "2.6.0", "last": "2.6.0", "codec": ""),
+    "cu126":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1"),
+    "cu128":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1"),
+    "cu129":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1"),
+    "cu130":     {"compat": list(systems.values()), "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1"),
     # ROCm
-    "rocm5.7":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.3.1", "last": "2.3.1"},
-    "rocm6.0":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.4.1", "last": "2.4.1"},
-    "rocm6.1":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.6.0", "last": "2.6.0"},
-    "rocm6.2":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.5.1", "last": "2.5.1"},
-    "rocm6.2.4": {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.7.1", "last": "2.11.0"},
-    "rocm6.3":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.7.1", "last": "2.9.1"},
-    "rocm7.0":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.10.0", "last": "2.10.0"},
-    "rocm7.1":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.11.0", "last": "2.11.0"},
-    "rocm7.2":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.11.0", "last": "2.11.0"},
-    "rocm-rel-7.2.1": {"compat": [systems['WINDOWS']], "base": "2.9.1", "last": "2.9.1"},
+    "rocm5.7":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.3.1", "last": "2.3.1", "codec": ""),
+    "rocm6.0":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.4.1", "last": "2.4.1", "codec": ""),
+    "rocm6.1":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.6.0", "last": "2.6.0", "codec": ""),
+    "rocm6.2":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.5.1", "last": "2.5.1", "codec": ""),
+    "rocm6.2.4": {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.7.1", "last": "2.7.1", "codec": ""),
+    "rocm6.3":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.7.1", "last": "2.9.1", "codec": ""),
+    "rocm7.0":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.10.0", "last": "2.10.0", "codec": "",
+    "rocm7.1":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.11.0", "last": "2.11.0", "codec": "",
+    "rocm7.2":   {"compat": [systems['LINUX'], systems['MACOS']], "base": "2.11.0", "last": "2.11.0", "codec": "",
+    "rocm-rel-7.2.1": {"compat": [systems['WINDOWS']], "base": "2.9.1", "last": "2.9.1", "codec": "",
     # MPS
-    "mps":       {"compat": [systems['MACOS']], "base": "2.7.1", "last": "2.11.0"},
+    "mps":       {"compat": [systems['MACOS']], "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1",
     # XPU
-    "xpu":       {"compat": [systems['LINUX'], systems['WINDOWS']], "base": "2.7.1", "last": "2.11.0"},
+    "xpu":       {"compat": [systems['LINUX'], systems['WINDOWS']], "base": "2.7.1", "last": "2.11.0", "codec": "0.11.1",
     # JETSON
-    "jetson51":  {"compat": [systems['LINUX']], "base": "2.4.1", "last": "2.4.1"},
-    "jetson60":  {"compat": [systems['LINUX']], "base": "2.4.0", "last": "2.4.0"},
-    "jetson61":  {"compat": [systems['LINUX']], "base": "2.5.0", "last": "2.5.0"}
+    "jetson51":  {"compat": [systems['LINUX']], "base": "2.4.1", "last": "2.4.1", "codec": "",
+    "jetson60":  {"compat": [systems['LINUX']], "base": "2.4.0", "last": "2.4.0", "codec": "",
+    "jetson61":  {"compat": [systems['LINUX']], "base": "2.5.0", "last": "2.5.0", "codec": ""
 }
 
 cuda_version_range = {"min": (11,8), "max": (13,0)}
