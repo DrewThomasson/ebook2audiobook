e48fd5b45352039d481bccb79455d6bce4c05042
unknown <admin@radsl.net>
2026-09-18 15:22:07 -0700

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -687,7 +687,7 @@ exit /b 0
 :check_device_info
 set "ARG=%~1"
 set "DEVICE_INFO_STR="
-for /f "delims=" %%I in ('"%PY_CMD%" -c "script_mode='%SCRIPT_MODE%'; import sys; from lib.classes.device_installer import DeviceInstaller as D; print(D().check_device_info(sys.argv[1]))" "%ARG%"') do set "DEVICE_INFO_STR=%%I"
+for /f "delims=" %%I in ('call "%PY_CMD%" -c "script_mode='%SCRIPT_MODE%'; import sys; from lib.classes.device_installer import DeviceInstaller as D; print(D().check_device_info(sys.argv[1]))" "%ARG%"') do set "DEVICE_INFO_STR=%%I"
 if not defined DEVICE_INFO_STR (
 	echo DEVICE_INFO_STR is empty
 	exit /b 1
