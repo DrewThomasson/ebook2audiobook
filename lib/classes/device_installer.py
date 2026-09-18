e0fbfe9431b1db31511518eba8a743845d1304dd
unknown <admin@radsl.net>
2026-09-18 15:09:45 -0700

    ...

--- lib/classes/device_installer.py
+++ lib/classes/device_installer.py
@@ -18,7 +18,7 @@ class DeviceInstaller():
         self.arch = self.check_arch
         self.python_version = sys.version_info[:2]
         self.python_version_tuple = sys.version_info
-        self._uv_bin = self._find_uv()
+        self.uv_bin = self._find_uv()
 
     def _find_uv(self)->str:
         p = shutil.which('uv')
