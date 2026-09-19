8fa9b5fa8afdff7f0b6515ee4ee9f7f13c7c561f
unknown <admin@radsl.net>
2026-09-19 08:42:42 -0700

    ...

--- ebook2audiobook.cmd
+++ ebook2audiobook.cmd
@@ -751,13 +751,13 @@ exit /b 0
 
 :install_device_packages
 "%PS_EXE%" %PS_ARGS% -Command ^
-"& $env:PY_CMD -c \"import sys, os; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); sys.exit(device.install_device_packages(os.environ.get('DEVICE_INFO_STR', '')))\""
+"& $env:"PY_CMD" -c \"import sys, os; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); sys.exit(device.install_device_packages(os.environ.get('DEVICE_INFO_STR', '')))\""
 exit /b %errorlevel%
 
 :install_python_packages
 echo Installing python dependencies…
 "%PS_EXE%" %PS_ARGS% -Command ^
-"& $env:PY_CMD -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); sys.exit(device.install_python_packages())\""
+"& "$env:PY_CMD" -c \"import sys; from lib.classes.device_installer import DeviceInstaller; device = DeviceInstaller(); sys.exit(device.install_python_packages())\""
 exit /b %errorlevel%
 
 :check_sitecustomized
@@ -999,7 +999,7 @@ if defined arguments.help (
 		call :check_sitecustomized
 		if errorlevel 1 goto :failed
 		call :build_gui
-		call uv run --no-project -- "%%PY_CMD%%" -u "%SAFE_SCRIPT_DIR%\app.py" --script_mode %SCRIPT_MODE% %ARGS%
+		call uv run --no-project -- "%PY_CMD%" -u "%SAFE_SCRIPT_DIR%\app.py" --script_mode %SCRIPT_MODE% %ARGS%
 	) else if "%SCRIPT_MODE%"=="%FULL_DOCKER%" (
 		call :check_sitecustomized
 		if errorlevel 1 goto :failed
